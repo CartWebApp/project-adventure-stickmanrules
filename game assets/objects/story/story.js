395ca1bf6ac9387f8a408bdaf46ad0a9682e75f9
DevinKC261 <devin26chanthirath@gmail.com>
2026-04-27 15:18:14 -0700

    Made and wrote the array for the story dialog
    The prologue is finished and a little of the first chapter

---
+++ game assets/objects/story/story.js
@@ -0,0 +1,21 @@
+const prolog =
+    [{
+        id: 1,
+        speaker: "Villager1", text: "Have you heard about the figure in the woods?",
+        speaker: "villager2", text: "They say he isolates himself in a hut in the abandoned woods and prays on the folks that go near his woods",
+        speaker: "villager3", text: "Stop joking, that story's old now everybody knows about the man in the woods",
+        speaker: "villager3", text: "Come here, lemme tell you the story of the figure that we- all the villagers of the figures rising remember"
+    }
+    ]
+
+
+const forest = [
+    {
+        id: 2,
+        speaker: "Narrator", text: "This so called figure is named 'Stickman' he wasn't like the rest of the villagers-unlike us who have fur and different perspectives, Stickman was flat and had no defining features",
+        speaker: "Narrator", text: "Because of his appearance, he grew bitter and outcasted himself from the villagers, no one really knows why but we really did try to help him but he just kept pushing us away",   
+        speaker: "Narrator", text: "",
+        speaker: "Narrator", text: "",
+        speaker: "Narrator", text: "",
+    }
+]
