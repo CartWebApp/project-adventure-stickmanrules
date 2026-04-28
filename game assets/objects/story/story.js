a46b2cc8b776ac26c7502d9a90f814a8b54d6007
Robbie-Netizen <robbie@tringali.me>
2026-04-28 15:49:11 -0700

    HTML, CSS and JS

--- game assets/objects/story/story.js
+++ game assets/objects/story/story.js
@@ -1,21 +1,39 @@
-const prolog =
-    [{
-        id: 1,
+const prolog = [ /*Begins will the villagers*/
+    {
+        id: p0,
         speaker: "Villager1", text: "Have you heard about the figure in the woods?",
         speaker: "villager2", text: "They say he isolates himself in a hut in the abandoned woods and prays on the folks that go near his woods",
         speaker: "villager3", text: "Stop joking, that story's old now everybody knows about the man in the woods",
         speaker: "villager3", text: "Come here, lemme tell you the story of the figure that we- all the villagers of the figures rising remember"
     }
-    ]
+]
 
-
-const forest = [
+const forest = [ /*The Narrator will tell most of the story*/
     {
-        id: 2,
+        id: p1,
         speaker: "Narrator", text: "This so called figure is named 'Stickman' he wasn't like the rest of the villagers-unlike us who have fur and different perspectives, Stickman was flat and had no defining features",
-        speaker: "Narrator", text: "Because of his appearance, he grew bitter and outcasted himself from the villagers, no one really knows why but we really did try to help him but he just kept pushing us away",   
-        speaker: "Narrator", text: "",
-        speaker: "Narrator", text: "",
-        speaker: "Narrator", text: "",
+        speaker: "Narrator", text: "Some say his parents died in a fire and he grew up resenting the ophanage he grewn up in",
+        speaker: "Narrator", text: "He rejected the towns folk as the towns folk rejected him which lead to them exiling him from the forest although they forgot about that",
+        speaker: "Narrator", text: "I don't know the exact details but I do know that he found a cabin in the woods to avoid others but ironically had to travel to the villages to get resources",
+    }
+]
+
+const forest_p1 = [
+    {
+        id: p2,
+        speaker: "Narrator", text: "Stickmans wandered around the forest after coming back from his outing in the nearby village, he looked exhausted taking heavy steps down the dirt trail",
+        speaker: "Narrator", text: "On the way back Stickman sees a strange hooded figure lurking around the trees"
+    }
+]
+
+const forest_a1 = [ /* Stickamn approachs the stranger */
+    {
+        id: fa1,
+        speaker: "Narrator", text: "Stickman cautiously approaches the figure from behind until he gets within a arms reach of the figure",
+        speaker: "Narrator", text: "*Pat*",
+        speaker: "The Stranger", text: "`WHO GOES THERE!?`",
+        speaker: "Narrator", text: "Stickman grabbed the strangers shoulder startling him as he quickly turned around meeting face to face with Stickman",
+        speaker: "The Stranger", text: "*Clears his throat* Excuss me, you caught me by surprise but as surprises go you're the one I'm looking for",
+        speaker: "Narrator", text: "Stickman looked puzzeled, someone was looking for him? He thought to himself "
     }
 ]
